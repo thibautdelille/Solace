85fe2871dca39b05cd5a5cf54aeeb49adbecf47b
Thibaut Delille <tdelille@gmail.com>
2024-11-01 15:08:36 -0700

    fix: typescript error in the api folder

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,10 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import db from '@/db';
+import { advocates } from '@/db/schema';
 
 export async function GET() {
   // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+  const data = await db.select().from(advocates);
 
-  const data = advocateData;
-
+  console.log(data.length);
   return Response.json({ data });
 }
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,16 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { Advocate } from '@/types';
+import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 
-const setup = () => {
+const setup = (): PostgresJsDatabase<Record<string, Advocate>> => {
   if (!process.env.DATABASE_URL) {
-    console.error("DATABASE_URL is not set");
-    return {
-      select: () => ({
-        from: () => [],
-      }),
-    };
+    console.error('DATABASE_URL is not set');
+    return drizzle(postgres(''));
   }
 
   // for query purposes
   const queryClient = postgres(process.env.DATABASE_URL);
-  const db = drizzle(queryClient);
+  const db: PostgresJsDatabase<Record<string, Advocate>> = drizzle(queryClient);
   return db;
 };
 
